5d2b5ed3b526ff3f1f2c284baa01fd3d16ebac52
Damir Galiakhmetov <damir11234@mail.ru>
2022-09-06 20:13:56 +0300

    Palindrome Number

--- src/Tests.java
+++ src/Tests.java
@@ -1,10 +1,16 @@
 import solutions.Task1;
 import solutions.Task13;
+import solutions.Task9;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tests {
     public static void main(String[] args) {
         //new Task13().test();
         //new Task1().test();
+        new Task9().test();
+
     }
 }
 
---
+++ src/solutions/Task9.java
@@ -0,0 +1,29 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task9 {
+    public static boolean isPalindrome(int x) {
+        List<Integer> result = new ArrayList<>();
+        if(x<0){
+            return false;
+        }
+        while (x>0){
+            result.add(x%10);
+            x = x/10;
+        }
+        for (int i = 0; i < result.size()/2; i++) {
+            if(result.get(i).equals(result.get(result.size()-i-1))){
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public void test() {
+        System.out.println(isPalindrome(-121));
+    }
+}
